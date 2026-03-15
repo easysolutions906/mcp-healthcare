8e8e220fa9b67dfa93655defd83b5a528f2aea31
easysolutions906 <easysolutions906@users.noreply.github.com>
2026-03-15 14:31:57 -0400

    Switch to production Stripe price IDs

--- src/stripe.js
+++ src/stripe.js
@@ -2,9 +2,9 @@ import Stripe from 'stripe';
 import { createKey, revokeKey, loadKeys } from './keys.js';
 
 const PRICE_TO_PLAN = {
-  'price_1TBJIuGsl682g3KowtPtegtx': 'starter',
-  'price_1TBJIuGsl682g3Ko47u4qphv': 'pro',
-  'price_1TBJIvGsl682g3Ko7L4ssKmw': 'business',
+  'price_1TBJRK2dvwjzjXFJeedIpEov': 'starter',
+  'price_1TBJRL2dvwjzjXFJzySh06SV': 'pro',
+  'price_1TBJRL2dvwjzjXFJSkCYjOAQ': 'business',
 };
 
 const PLAN_TO_PRICE = Object.fromEntries(
